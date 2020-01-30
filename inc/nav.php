98003a9d3b5636a6618fcffedda6124d486a3ada
Jose Fandiño <piolinfan07@gmail.com>
2020-01-29 22:27:20 -0500

    commit Initial

---
+++ inc/nav.php
@@ -0,0 +1,16 @@
+<header class="header">
+   <input type="checkbox" name="" id="btnMenu">
+   <label for="btnMenu"><i class="fas fa-bars"></i></label>
+
+   <nav class="menu">      
+      <ul>
+         <li><a href="">Inicio</a></li>
+         <li><a href="">Services</a></li>
+         <li><a href="">Prodcts</a></li>
+         <li><a href="">Gallery</a></li>
+         <li><a href="">Contact</a></li>
+      </ul>
+   </nav>
+</header>
+
+   
---
+++ index.php
@@ -0,0 +1,6 @@
+<?php include_once 'inc/header.php';?>
+<body>
+   <?php include_once 'inc/nav.php'; ?>
+
+</body>
+</html>
