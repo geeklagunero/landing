f122eed49e3f4ff3dba8ee853ef79f4939f78537
Jose Fandiño <piolinfan07@gmail.com>
2020-01-30 11:05:41 -0500

    LandingPage section 1,2,3 casi terminada

--- index.php
+++ index.php
@@ -3,20 +3,58 @@
    <?php include_once 'inc/nav.php'; ?>
 
    <section class="banner">
-      <h1></h1>
-      <p></p>
-   </section>
-
-   <section class="products">
-      <div class="childProducts">
-         <p></p>
-         <img src="" alt="">
+      <div class="bgBanner">
+         <div class="childBanner">
+            <h1>RJ Developer</h1>
+            <p>Estamos aprendiendo Frontend</p>
+            <a href="#">Contactar</a>
+         </div>
       </div>
    </section>
 
+   <section class="products">
+      <div id="products"></div>
+      <div class="childProducts">
+         <p>Sale</p>
+         <img src="assets/img/cat-3.jpg" alt="">
+      </div>
+
+      <div class="childProducts">
+         <p>Accesories</p>
+         <img src="assets/img/cat-1.jpg" alt="">
+      </div>
+
+      <div class="childProducts">
+         <p>Shoes</p>
+         <img src="assets/img/cat-4.jpg" alt="">
+      </div>
+
+      <div class="childProducts">
+         <p>New</p>
+         <img src="assets/img/cat-5.jpg" alt="">
+      </div>
+
+      <div class="childProducts">
+         <p>Woemen</p>
+         <img src="assets/img/cat-2.jpg" alt="">
+      </div>
+
+      <div class="childProducts">
+         <p>Kids</p>
+         <img src="assets/img/cat-6.jpg" alt="">
+      </div>
+
+   </section>
+
    <section class="aboutMe">
-      <div class="childAbout"></div>
-      <div class="childAbout"></div>
+      <div id="services"></div>
+      <div class="childAbout"><img src="assets/img/aboutme.jpg" alt=""></div>
+      <div class="childAbout">
+         <h2>Siempre a la moda</h2>
+         <p><b>Productos exclusivos para ti que eres única</b></p>
+         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore amet voluptatibus iusto eius officia ratione, aperiam ducimus cum, incidunt aliquid, accusamus dolorem! Et nulla ab voluptate ex, perferendis modi. In.</p>
+         <a href="#">Detalles</a>
+      </div>
    </section>
    hasta aqui trabajo yo
 
