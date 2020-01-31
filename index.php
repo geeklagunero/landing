5a04c28279a8fb65990f057fe3b3ff05c77393a5
Jose Fandiño <websysfan@gmail.com>
2020-01-31 12:31:12 -0500

    corrección centrado de texto

--- index.php
+++ index.php
@@ -14,41 +14,56 @@
 
    <section class="products">
       <div id="products"></div>
-      <div class="childProducts">
-         <p>Sale</p>
-         <img src="assets/img/cat-3.jpg" alt="">
-      </div>
+      
+         <div class="childProducts">
+            <div class="boxProducts">
+               <div class="textProducts"><p>Sale</p></div>
+               <img class="img1" src="assets/img/cat-3.jpg" alt="">
+            </div>
+         </div>
 
-      <div class="childProducts">
-         <p>Accesories</p>
-         <img src="assets/img/cat-1.jpg" alt="">
-      </div>
+         <div class="childProducts">
+            <div class="boxProducts">
+               <div class="textProducts"><p>Accesories</p></div>
+               <img class="img1" src="assets/img/cat-1.jpg" alt="">
+            </div>
+         </div>
 
-      <div class="childProducts">
-         <p>Shoes</p>
-         <img src="assets/img/cat-4.jpg" alt="">
-      </div>
+         <div class="childProducts">
+            <div class="boxProducts">
+               <div class="textProducts"><p>Shoes</p></div>
+               <img class="img1" src="assets/img/cat-4.jpg" alt="">
+            </div>
+         </div>
 
-      <div class="childProducts">
-         <p>New</p>
-         <img src="assets/img/cat-5.jpg" alt="">
-      </div>
+         <div class="childProducts">
+            <div class="boxProducts">
+               <div class="textProducts"><p>NEW</p></div>
+               <img class="img1" src="assets/img/cat-5.jpg" alt="">
+            </div>
+         </div>
 
-      <div class="childProducts">
-         <p>Woemen</p>
-         <img src="assets/img/cat-2.jpg" alt="">
-      </div>
+         <div class="childProducts">
+            <div class="boxProducts">
+               <div class="textProducts"><p>Woemen</p></div>
+               <img class="img1" src="assets/img/cat-2.jpg" alt="">
+            </div>
+         </div>
 
-      <div class="childProducts">
-         <p>Kids</p>
-         <img src="assets/img/cat-6.jpg" alt="">
-      </div>
-
+         <div class="childProducts">
+            <div class="boxProducts">
+               <div class="textProducts"><p>Kids</p></div>
+               <img class="img1" src="assets/img/cat-6.jpg" alt="">
+            </div>
+         </div>
    </section>
 
    <section class="aboutMe">
       <div id="services"></div>
-      <div class="childAbout"><img src="assets/img/aboutme.jpg" alt=""></div>
+      <div class="childAbout">
+         <img src="assets/img/aboutme.jpg" alt="">
+      </div>
+
       <div class="childAbout">
          <h2>Siempre a la moda</h2>
          <p><b>Productos exclusivos para ti que eres única</b></p>
@@ -56,6 +71,8 @@
          <a href="#">Detalles</a>
       </div>
    </section>
+
+   
    hasta aqui trabajo yo
 
    <!-- ------------------------------------------ -->
