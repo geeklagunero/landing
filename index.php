4829ef99e0cdd5745e976a51d56c3eb1e4515194
Ricardo Roman <ricardomon85@gmail.com>
2020-02-04 19:17:29 -0600

    Subir seccion 3 y 4 pendiente footer

--- index.php
+++ index.php
@@ -2,5 +2,103 @@
 <body>
    <?php include_once 'inc/nav.php'; ?>
 
+
+<!-- ricardo changes --->
+<section class="days">
+        <div class="conteiner">
+            
+            <div class="texto">
+                <h3>Fashion`s Days</h3>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut soluta dolor illum amet, consectetur enim commodi accusantium et aperiam saepe similique expedita, quos repellendus, maxime sit repudiandae nostrum optio velit.</p>
+                <a href="#">More Info</a>
+            </div>
+            <div class="imagen">
+                <div class="contenedor-img">
+                    <img src="assets/img/woman.jpg" alt="">
+                </div>
+            </div>
+        </div>
+    </section>
+   
+    <div class="sub-titulo">
+        <h2>Our Services</h2>
+    </div>
+
+    <section class="service">
+        
+        <div class="conteiner2">
+            
+            <div class="item">
+                <div class="icono">
+                    <i class="fas fa-pen"></i>
+                </div>
+                <div class="texto-der">
+                    <h2>Titulo del Item</h2>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                </div>
+            </div>
+            
+            <div class="item">
+                <div class="icono">
+                    <i class="fas fa-paper-plane"></i>
+                </div>
+                <div class="texto-der">
+                    <h2>Titulo del Item</h2>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta sequi similique, asperiores, </p>
+                </div>
+            </div>
+
+            <div class="item">
+                <div class="icono">
+                    <i class="far fa-address-book"></i>
+                </div>
+                <div class="texto-der">
+                    <h2>Titulo del Item</h2>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta sequi similique, asperiores.</p>
+                </div>
+            </div>
+
+            <div class="item">
+                <div class="icono">
+                    <i class="fab fa-apple"></i>
+                </div>
+                <div class="texto-der">
+                    <h2>Titulo del Item</h2>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta sequi similique</p>
+                </div>
+            </div>
+
+            <div class="item">
+                <div class="icono">
+                    <i class="fab fa-android"></i>
+                </div>
+                <div class="texto-der">
+                    <h2>Titulo del Item</h2>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta sequi similique</p>
+                </div>
+            </div>
+
+            <div class="item">
+                <div class="icono">
+                    <i class="fas fa-wifi"></i>
+                </div>
+                <div class="texto-der">
+                    <h2>Titulo del Item</h2>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta sequi similique</p>
+                </div>
+            </div>
+
+        </div>
+    </section>
+
+    <!--- Footer -->
+    <footer>
+        <div class="conteiner">
+            <div class="menu-footer">
+
+            </div>
+        </div>
+    </footer>
+
 </body>
 </html>
