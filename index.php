8d6408c9ab751e601c9ed6ca6d0574e60c2d59a3
Jose Fandiño <websysfan@gmail.com>
2020-02-07 08:31:14 -0500

    proyecto a punto de finalizar

--- index.php
+++ index.php
@@ -76,11 +76,157 @@
    </section>
 
    
-   hasta aqui trabajo yo
-
-   <!-- ------------------------------------------ -->
-   desde aqui empieza tu codigo
+   <!-- ricardo changes --->
+<section class="days">
+    <div class="conteiner1">
+        <div class="texto">
+            <h3>Fashion`s Days</h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut soluta dolor illum amet, consectetur enim commodi accusantium et aperiam saepe similique expedita, quos repellendus, maxime sit repudiandae nostrum optio velit.</p>
+            <a href="#">More Info</a>
+        </div>
+        <div class="imagen">
+            <img src="assets/img/woman.jpg" alt="">
+        </div>
+    </div>
+</section>
    
+    <div class="sub-titulo">
+        <h2>Our Services</h2>
+    </div>
+
+    <section class="service">
+        
+        <div class="conteiner2">
+            
+            <div class="item">
+                <div class="icono">
+                    <i class="fas fa-pen"></i>
+                </div>
+                <div class="texto-der">
+                    <h2>Titulo del Item</h2>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                </div>
+            </div>
+            
+            <div class="item">
+                <div class="icono">
+                    <i class="fas fa-paper-plane"></i>
+                </div>
+                <div class="texto-der">
+                    <h2>Titulo del Item</h2>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta sequi similique, asperiores, </p>
+                </div>
+            </div>
+
+            <div class="item">
+                <div class="icono">
+                    <i class="far fa-address-book"></i>
+                </div>
+                <div class="texto-der">
+                    <h2>Titulo del Item</h2>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta sequi similique, asperiores.</p>
+                </div>
+            </div>
+
+            <div class="item">
+                <div class="icono">
+                    <i class="fab fa-apple"></i>
+                </div>
+                <div class="texto-der">
+                    <h2>Titulo del Item</h2>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta sequi similique</p>
+                </div>
+            </div>
+
+            <div class="item">
+                <div class="icono">
+                    <i class="fab fa-android"></i>
+                </div>
+                <div class="texto-der">
+                    <h2>Titulo del Item</h2>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta sequi similique</p>
+                </div>
+            </div>
+
+            <div class="item">
+                <div class="icono">
+                    <i class="fas fa-wifi"></i>
+                </div>
+                <div class="texto-der">
+                    <h2>Titulo del Item</h2>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta sequi similique</p>
+                </div>
+            </div>
+
+        </div>
+    </section>
+
+    <!-- Barra Redes Sociales-->
+    <div class="barra">
+        <div class="contenedor-barra">
+            <div class="formulario">
+                <p>Newsletter sign up</p>
+                <input type="text">
+                <p><a href="#">Subscribete</a></p>
+            </div>
+            <div class="redes">
+                <i class="fab fa-google-plus-square"></i>
+                <i class="fab fa-facebook-square"></i>
+                <i class="fab fa-twitter-square"></i>
+                <i class="fab fa-youtube-square"></i>
+            </div>
+        </div>
+    </div>
+
+    <!--- Footer -->
+    <footer>
+        <div class="conteiner-footer">
+            <div class="menu-footer">
+                <div class="item2 about">
+                    <h2>About</h2>
+                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Impedit natus, sit quas unde voluptates minus optio vel fugit </p>
+                </div>
+                <div class="item2 category">
+                    <h2>Category</h2>
+                    <ul>
+                        <li>Accesories</li>
+                        <li>Men</li>
+                        <li>Jackets</li>
+                        <li>Woman</li>
+                        <li>Shoes</li>
+                        <li>Shorts</li>
+                    </ul>
+                </div>
+                <div class="item2 info">
+                    <h2>The Service</h2>
+                    <ul>
+                        <li>My Account</li>
+                        <li>Order History</li>
+                        <li>Vendor contact</li>
+                        <li>Shop</li>
+                        <li>Search</li>
+                    </ul>
+                </div>
+                <div class="item2 services">
+                    <h2>Information</h2>
+                    <ul>
+                        <li>About Us</li>
+                        <li>New Collection</li>
+                        <li>Bestsellers</li>
+                        <li>Manufactures</li>
+                        <li>Privacy pollcy</li>
+                        <li>Term & condition</li>
+                    </ul>
+                </div>
+                <div class="item2 contac">
+                    <h2>Contact Us</h2>
+                    <p><i class="fas fa-map-marker-alt"></i> <span>Calle Septima</span></p>
+                    <p><i class="fas fa-phone-alt"></i><span>(871) 8767898</span></p>
+                    <p><i class="fas fa-envelope"></i></i> <span>contact@gmail.com</span></p>
+                </div>
+            </div>
+        </div>
+    </footer>
 
 </body>
 </html>
